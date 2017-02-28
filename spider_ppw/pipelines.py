6af9fe8a9a5bff1f09d24d36af7065e7efd58538
czk <chenzikunczk@gmail.com>
2017-02-28 19:27:58 +0800

    写入数据库(++)

--- spider_ppw/pipelines.py
+++ spider_ppw/pipelines.py
@@ -51,7 +51,7 @@ class CustomRedisPipeline(RedisPipeline):
         self.num += self.num + 1
         _item = self.handle_item(item)
         sql = self.format_spl(_item)
-
+        self.db.insert_mysql(sql)
         print('[INFO]: ')
 
         if isinstance(item, SpiderPpwItemTransfer):
