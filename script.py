f63f96fe8b9fba9c5a67e2c0f3d46b9412815d95
czk <chenzikunczk@gmail.com>
2017-03-15 19:48:41 +0800

    增加计划任务

--- script.py
+++ script.py
@@ -2,16 +2,13 @@ import os
 os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'spider_ppw.settings')
 from datetime import datetime
 from atexit import register
-import redis
 
 # 计划任务
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-
 from scrapy.crawler import CrawlerProcess
 from scrapy.conf import settings
-from spider_ppw.spiders import GanJiSpider
-
+from spider_ppw.spiders.ganji import GanJiSpider
 from spider_ppw.constant import proxy_verify
 from spider_ppw.constant import ip_pond
 
@@ -19,29 +16,57 @@ from spider_ppw.constant import ip_pond
 process = CrawlerProcess(settings)
 
 
-def date_time():
-    return datetime.now().strftime('%H:%M:%S')
+class Main(object):
 
+    def __init__(self):
+        # ip池爬取次数计数
+        self.ip_pond_num = 0
+        # ip池验证计数数
+        self.verify_ip_pond_num = 0
+        # 主爬虫运行计数
+        self.crawl_num = 0
 
-# def test_ip_pond():
-#     sched = BlockingScheduler()
-#     sched.add_job(, 'interval', seconds=180)
+    @staticmethod
+    def date_time():
+        return datetime.now().strftime('%H:%M:%S')
+
+    def refresh_ip_pond(self):
+        self.ip_pond_num += 1
+        print('第{}次更新ip_pond: {}'.format(self.ip_pond_num, self.date_time()))
+        ip_pond.Manager()
+        print('ip池爬取完毕')
+
+    def test_ip_pond(self):
+        self.verify_ip_pond_num += 1
+        print('第{}次更新ip_pond: {}'.format(self.verify_ip_pond_num, self.date_time()))
+        proxy_verify.VerifyProxy()
+        print('ip池验证完毕')
+
+    def crawl(self):
+        self.crawl_num += 1
+        print('#' * 60)
+        print(format('  第{}次进行主爬虫程序: {}  '.format(self.crawl_num, self.date_time())), '#^60')
+        print('#' * 60)
+        process.crawl(GanJiSpider)
+        process.start()
+
 
 if __name__ == '__main__':
-    ip_pond.Manager()
-    print('ip池爬取完毕')
-    proxy_verify.VerifyProxy()
-
-    # 爬取进程
-    process.crawl(GanJiSpider)
-    print('!'*40)
-    print('爬虫开始：{}'.format(date_time()))
-    print('!' * 40)
-    process.start()
+    main = Main()
+    sched = BlockingScheduler()
+    sched.add_job(main.refresh_ip_pond, 'interval', minutes=20)
+    sched.add_job(main.test_ip_pond, 'interval', minutes=5)
+    sched.add_job(main.crawl, 'interval', minutes=30)
+    try:
+        print('开始计划任务')
+        sched.start()
+    except Exception as e:
+        print(e)
+        print('计划任务终止')
 
 
 @register
 def _at_exit():
-    redis_conn = redis.StrictRedis.from_url(settings.get('REDIS_URL'))
-    redis_conn.flushdb()
-    print('爬虫结束：{}'.format(date_time()))
+    print('#' * 60)
+    print(format('    爬虫结束：{}    '.format(main.date_time()), '^60'))
+    print('#' * 60)
--- spider_ppw/constant/proxy_verify.py
+++ spider_ppw/constant/proxy_verify.py
@@ -3,7 +3,7 @@ import requests
 from multiprocessing import Process
 
 
-class VerifyProxy():
+class VerifyProxy(object):
 
     def __init__(self):
         self.redis_db = RedisDatabase()
@@ -15,24 +15,23 @@ class VerifyProxy():
         print('筛选后ip池个数： {}'.format(len(self.ip_pond)))
         self.redis_db.refresh_ip_pond(self.ip_pond)
 
-
     def very_proxy(self, ip, port):
         # print(ip, port)
-        # proxies = {"http": "http://{}:{}".format(ip, port)}
-        proxies = {"http": "http://{}:{}".format(ip, port),
-                   "https": "https://{}:{}".format(ip, port)}
+        proxies = {"http": "http://{}:{}".format(ip, port)}
+        # proxies = {"http": "http://{}:{}".format(ip, port),
+        #            "https": "https://{}:{}".format(ip, port)}
         try:
-            r = requests.get('https://www.baidu.com/', proxies=proxies, timeout=20, verify=False)
+            r = requests.get('https://www.baidu.com/', proxies=proxies, timeout=30, verify=False)
             if r.status_code == 200:
                 self.ip_pond.append((ip, port))
-        except:
-            pass
-
+        except Exception as e:
+            print('(ip {}:{} 未通过测试'.format(ip, port))
+            print(e)
 
     def main(self):
-        process_list = [Process(target=self.very_proxy, args=(self.ip_pond.pop())) for item in range(len(self.ip_pond))]
+        process_list = [Process(target=self.very_proxy, args=(self.ip_pond[index]))
+                        for index in range(len(self.ip_pond))]
         for p in process_list:
             p.start()
         for p in process_list:
             p.join()
-
